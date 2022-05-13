8b17ec88c95f92ad9118514fec35dc74e9920206
Amado-Melguizo <amado.melgizo@gmail.com>
2022-05-13 18:02:16 +0200

    automateCommit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import URL from "./targetProcess.js";
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();
-
+//a
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
