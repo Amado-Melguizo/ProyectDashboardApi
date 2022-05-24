88bca40240d7cd479e1cf7954095cac61dbbfc54
Amado-Melguizo <amado.melgizo@gmail.com>
2022-05-24 13:56:31 +0200

    [Refactorizacion_y_Funcionalidad]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import URL from "./targetProcess.js";
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();
-//prueba 3.5
+//prueba 3.6
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
